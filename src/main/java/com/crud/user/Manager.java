5539d0696e444a29536504e30d3238c9562b9c48
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2024-08-22 10:48:56 +0700

    get param from url

--- src/main/java/com/crud/user/Manager.java
+++ src/main/java/com/crud/user/Manager.java
@@ -37,23 +37,48 @@ public final class Manager extends HttpServlet {
 		// TODO Auto-generated method stub
 //		super.service(req, resp);
 //		System.out.println("This is manager.");
+		
+		String func = "";
 		String err = "success";
-		HttpSession session = null;
-		session = request.getSession(false);
-		if (session == null) {
-			err = "Session is NULL";
-			response.sendRedirect(request.getContextPath() + "/login/login.jsp?invalid=1");
-			logger.info(err);
-			return;
-		}
+//		HttpSession session = null;
+//		session = request.getSession(false);
+//		if (session == null) {
+//			err = "Session is NULL";
+//			response.sendRedirect(request.getContextPath() + "/login/login.jsp?invalid=1");
+//			logger.info(err);
+//			return;
+//		}
 		
 		String reqURL= request.getQueryString();
 		reqURL=reqURL.replaceAll(",", ";");
 		System.out.println(">>>>>>>>> request URL: " + reqURL);
-
+	     
 		try {
 		      response.setContentType("text/html; charset=utf-8");
-		      String ssId=session.getId();
+//		      String ssId=session.getId();
+		      
+		   // lay param tren url, sau do lay duong dan file htm
+				String cPath = request.getContextPath();
+				String appPath = getServletContext().getRealPath(request.getRequestURI());
+				System.out.println(">>>>>>>>> cPath: " + cPath + ", appPath: " + appPath);
+				if(appPath==null) {
+					appPath = getServletContext().getRealPath("/");
+					appPath=appPath.replace('\\','/')+"main/";
+				} else {
+					appPath=appPath.replace('\\','/');
+					if ( cPath.length() > 1 )
+					{
+					   int f1 = appPath.lastIndexOf(cPath);
+					   appPath = appPath.substring(0,f1) + appPath.substring(f1 + cPath.length());
+					}
+					appPath = appPath.substring(0,appPath.lastIndexOf('/')+1);
+				}
+			
+			     func = request.getParameter("func");
+			     
+			     String sTemplateFileName = func+".htm"; 
+			     System.out.println(">>>>>>>>>>>>> sTemplateFileName: " + sTemplateFileName );
+			  // lay param tren url, sau do lay duong dan file htm
 		} catch (Exception t) {
 	    	
 	    	logger.info("ERROR-->LOGIN");
